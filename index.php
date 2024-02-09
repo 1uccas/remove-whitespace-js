f3687514eb9359fb2c5b36efafdc1904da19bb26
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-09 17:30:15 -0300

    feat: add new class master, navbar, container-lg

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<body class="bg_dark">
+	<main class="class-master">
+		<div class="navbar m-3">
+			<div class="container-lg">
+				<input type="text" name="input_text" id="input" class="form-control w-100" value="" required="required" pattern="" title="">
+				<button type="button" id="button_submit" class="btn btn-warning">button</button>	
+			</div>
+		</div>
+	</main>
+</body>
