fa1ba1608d00374c5b6cbb9ac22457f804db73c5
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-09 17:36:31 -0300

    feat: add new form
    * - remove div container-fluid

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <body class="bg_dark">
 	<main class="class-master">
 		<div class="navbar m-3">
-			<div class="container-lg">
-				<input type="text" name="input_text" id="input" class="form-control w-100" value="" required="required" pattern="" title="">
+			<form action="index.php" method="POST" class="container-fluid">
+				<input type="text" name="input_text" id="input" class="form-control" value="" required="required" pattern="" title="">
 				<input type="text" name="input_text_result" id="input" class="form-control mt-3 mb-3 w-100" value="" required="required" pattern="" title="">
-				<button type="button" id="button_submit" class="btn btn-warning">button</button>	
-			</div>
+				<button type="button" id="button_submit" class="btn btn-warning">Enter</button>
+			</form>
 		</div>
 	</main>
 </body>
