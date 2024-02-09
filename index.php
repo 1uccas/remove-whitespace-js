a6c3efccd999082c5cc0ca6db82b8c17c668a60a
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-09 18:26:18 -0300

    feat: add mod dark bootstrap in body

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function returnText(){
 ?>
 <link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<body class="bg_dark">
+<body class="bg-dark">
 	<main class="class-master">
 		<div class="navbar m-3">
 			<form action="index.php" method="POST" class="container-fluid">
