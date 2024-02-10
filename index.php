8f932b1d77a966aad523692553ecff158198af8a
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-09 21:29:11 -0300

    feat: add new placeholder in input
    * - remove class body
    
    * - update link style css

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ function returnText(){
 		$resultText = $_POST['input_text_result'];
 
 		$newText = str_replace(" ","",$text);
-
 		return $newText;
 	}else{
 		return die("404");
@@ -15,14 +14,14 @@ function returnText(){
 
 
 ?>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="src/style/style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<body class="bg-dark">
+<body>
 	<main class="class-master">
 		<div class="navbar m-3">
 			<form action="index.php" method="POST" class="container-fluid">
-				<input type="text" name="input_text" id="input" class="form-control" required="required" >
-				<input type="text" name="input_text_result" id="resultText" class="form-control mt-3 mb-3 w-100" value="<?php echo returnText(); ?>" ><button id="copy" type="button" class="btn btn-primary">copy</button>
+				<input type="text" name="input_text" id="input" class="form-control" required="required" placeholder="Enter your text">
+				<input type="text" name="input_text_result" id="resultText" class="form-control mt-3 mb-3 w-100" value="<?php echo returnText(); ?>" placeholder="Result"><button id="copy" type="button" class="btn btn-primary">copy</button>
 				<button type="submit" id="button_submit" class="btn btn-warning w-75">Enter</button>
 			</form>
 		</div>
